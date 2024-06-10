410f31c40f910d398186aa079499759e8bd7d3c3
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2024-06-10 11:02:28 +0600

    verifyAdmin

--- index.js
+++ index.js
@@ -52,6 +52,18 @@ async function run() {
         req.decoded = decoded;
         next();
       })
+    }    
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
     }
 
     
