a38ba0f04dc80462a1bffbd3b582779a026ebe3f
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2024-06-12 12:39:42 +0600

    delete /coupons/:id

--- index.js
+++ index.js
@@ -170,6 +170,15 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/coupons/:id', async(req,res)=>{
+      const id = req.params.id
+
+      const result = await couponsCollection.deleteOne({_id: new ObjectId(id)})
+      
+      console.log(result)
+      res.send(result) 
+    })
+
       // Send a ping to confirm a successful connection
       // await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
